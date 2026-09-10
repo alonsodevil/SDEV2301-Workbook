28dc5f3e146ef9d2d2cc5445cbbf146eea64b1a4
alonsodevil <alonsomolly22@gmail.com>
2026-09-10 09:20:05 -0600

    while loop

---
+++ lesson 3 - loops & if/loops/whileloop.cs
@@ -0,0 +1,21 @@
+// does not enter the while loop
+
+int count = 5;
+
+while (count < 3)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
+/* ---------------------------------- brub ---------------------------------- */
+
+// exclusive condition
+count = 1;
+
+while (count < 3)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
