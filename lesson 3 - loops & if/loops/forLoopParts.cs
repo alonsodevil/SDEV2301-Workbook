55f461041b73d8454c4a13c98f5a496ec09fbe1c
alonsodevil <alonsomolly22@gmail.com>
2026-09-10 09:09:26 -0600

    loop parts

---
+++ lesson 3 - loops & if/loops/forLoopParts.cs
@@ -0,0 +1,17 @@
+int count = 1;
+
+while (count <= 5)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
+// ? this is how a for loop works
+// ? it is the same as this 
+// ?        |
+// ?        v
+
+for (int count2 = 1; count2 <= 5; count2++)
+{
+    Console.WriteLine(count);
+}
