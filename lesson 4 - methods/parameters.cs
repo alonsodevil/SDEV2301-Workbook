ae46c3c52bca8f9d4f9658340e9090b17939030f
alonsodevil <alonsomolly22@gmail.com>
2026-09-14 10:58:33 -0600

    Parameters

--- lesson 4 - methods/parameters.cs
+++ lesson 4 - methods/parameters.cs
@@ -3,4 +3,10 @@ static int Multiply(int x, int y)
     return x * y; 
 }
 
-Console.WriteLine(Multiply(4, 5));
+int a = 12;
+int b = 2;
+
+int result = Multiply(12, 2);
+
+Console.WriteLine($"{a} * {b} = {result}.");
+
